0eeab25f6cf6da055c923f2e359c090926fdbb6c
Vsevolod Kaspirovich <kaspirovich.vm@phystech.edu>
2023-12-13 22:59:53 +0300

    some adds

--- Assets/Scripts/MapGenerator/TilemapPainter.cs
+++ Assets/Scripts/MapGenerator/TilemapPainter.cs
@@ -5,38 +5,95 @@ using UnityEngine.Tilemaps;
 
 public class TilemapPainter : MonoBehaviour
 {
-    public Tile wallTile;
-    public Tilemap tilemap;
+    public Tilemap wall;
+    public Tilemap floor;
+
+    public List<Tile> wallTiles;
+    public List<Tile> sideTiles;
+    public List<Tile> floorTiles;
 
     public void PaintMap(int[,] map)
     {
+        PreprocessMap(map);
         int width = map.GetLength(0);
         int height = map.GetLength(1);
         CenterTilemap(width, height);
-        tilemap.ClearAllTiles();
+        wall.ClearAllTiles();
         for (int x = 0; x < map.GetLength(0); x++)
         {
             for (int y = 0; y < map.GetLength(1); y++)
             {
                 if (map[x, y] == 1)
                 {
-                    tilemap.SetTile(new Vector3Int(x, y, 0), wallTile);
+                    int idx = Random.Range(0, wallTiles.Count - 1);
+                    wall.SetTile(new Vector3Int(x, y, 0), wallTiles[idx]);
+                }
+                else if (map[x, y] == 2)
+                {
+                    int idx = Random.Range(0, sideTiles.Count - 1);
+                    wall.SetTile(new Vector3Int(x, y, 0), sideTiles[idx]);
+                }
+            }
+        }
+        PostProcessMap(width, height);
+        PaintFloor(width, height);
+    }
+
+    public void CenterTilemap(int width, int height)
+    {
+        wall.GetComponent<Transform>().position = new Vector3(-width / 2, -height / 2, 0);
+        floor.GetComponent<Transform>().position = new Vector3(-width / 2, -height / 2, 0);
+    }
+
+    public void ClearMap()
+    {
+        wall.ClearAllTiles();
+    }
+
+
+    void PreprocessMap(int[,] map) {
+        for (int x = 0; x < map.GetLength(0); x++)
+        {
+            for (int y = 1; y < map.GetLength(1); y++)
+            {
+                if (map[x, y] == 1 && map[x, y - 1] == 0) {
+                    map[x, y] = 2;
                 }
             }
         }
     }
 
-    public void CenterTilemap(int width, int height)
+    void PaintFloor(int width, int height)
     {
-        tilemap.GetComponent<Transform>().position = new Vector3(-width / 2, -height / 2, 0);
+        floor.ClearAllTiles();
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                int idx = Random.Range(0, floorTiles.Count - 1);
+                floor.SetTile(new Vector3Int(x, y, 0), floorTiles[idx]);
+            }
+        }
     }
 
-    public void ClearMap()
-    {
-        tilemap.ClearAllTiles();
+    void PostProcessMap(int width, int height, int offset = 20) {
+        /*FillWall(-10, -10, width + 10, 0);
+        FillWall(-10, 0, 0, height + 10);
+        FillWall(width, 0, width + 10, height + 10);
+        FillWall(0, height, width, height + 10);*/
+        FillWall(-offset, -offset, width + offset, 0);
+        FillWall(-offset, 0, 0, height + offset);
+        FillWall(width, 0, width + offset, height + offset);
+        FillWall(0, height, width, height + offset);
     }
 
-
-
+    void FillWall(int x1, int y1, int x2, int y2) {
+        for (int x = x1; x < x2; x++) {
+            for (int y = y1; y < y2; y++) {
+                int idx = Random.Range(0, wallTiles.Count - 1);
+                wall.SetTile(new Vector3Int(x, y, 0), wallTiles[idx]);
+            }
+        }
+    }
 
 }
