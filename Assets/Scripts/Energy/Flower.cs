0f0714e7bce875ef1a8543b7fedc373e45832767
voroninip <voronin.ip@phystech.edu>
2023-12-20 17:19:29 +0300

    Ok

---
+++ Assets/Scripts/Energy/Flower.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flower : MonoBehaviour
+{
+    private float energy = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(this);
+    }
+
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+  public float moveSpeed = 5.0f;
+
+  public Rigidbody2D rb;
+  public Animator animator;
+
+  private Vector2 movement;
+
+  [SerializeField] public List<PlayerGun> Guns = new List<PlayerGun>();
+  [SerializeField] private PlayerGun gun;
+  public int selectedWeapon;
+
+  private bool isShooting = false;
+  public UpgradeProducts upgradeProducts;
+  public float health = 20.0f;
+  float damageMultiplier = 1.0f;
+
+  private void Start()
+  {
+    gun = Guns[0];
+    if (PlayerPrefs.HasKey("health") == false)
+    {
+      PlayerPrefs.SetInt("health", 0); // пример значения
+    }
+    if (PlayerPrefs.HasKey("damage") == false)
+    {
+      PlayerPrefs.SetInt("damage", 0); // пример значения
+    }
+    StatusUpdate();
+  }
+
+  private void HandleMovementInput()
+  {
+    movement.x = Input.GetAxisRaw("Horizontal");
+    movement.y = Input.GetAxisRaw("Vertical");
+
+    animator.SetFloat("Horizontal", movement.x);
+    animator.SetFloat("Vertical", movement.y);
+    animator.SetFloat("Speed", movement.sqrMagnitude);
+  }
+
+  private void Update()
+  {
+    selectedWeapon = WeaponSwitching.selectedWeapon;
+    gun = Guns[selectedWeapon];
+
+    HandleMovementInput();
+    gun.RotateWeapon();
+
+    if (Input.GetMouseButtonDown(0))
+    {
+      isShooting = true;
+    }
+    else if (Input.GetMouseButtonUp(0))
+    {
+      isShooting = false;
+    }
+
+    if (isShooting)
+    {
+      gun.Shoot();
+    }
+  }
+
+
+
+  public void TakeDamage(float damage)
+  {
+    health -= damage;
+
+
+    if (health <= 0)
+    {
+      /*PlayerPrefs.DeleteKey("health");
+      PlayerPrefs.DeleteKey("damage");
+      if (PlayerPrefs.HasKey("ups") == false)
+      {
+        PlayerPrefs.SetInt("ups", 0); // пример значения
+      }
+      int ups = PlayerPrefs.GetInt("ups");
+      PlayerPrefs.DeleteKey("ups");
+      PlayerPrefs.SetInt("ups", ups + 1);
+*/
+      SceneManager.LoadScene("Upgrade");
+
+      Destroy(gun);
+      Destroy(gameObject);
+
+    }
+  }
+
+  public void StatusUpdate()
+  {
+    if (PlayerPrefs.HasKey("health"))
+    {
+      health = 20 + 5 * PlayerPrefs.GetInt("health");
+    }
+    if (PlayerPrefs.HasKey("damage"))
+    {
+      int damage = PlayerPrefs.GetInt("damage");
+      damageMultiplier = 1.0f * (float)(Math.Pow(1.2f, damage));
+    }
+
+    selectedWeapon = WeaponSwitching.selectedWeapon;
+    gun = Guns[selectedWeapon];
+    gun.SetMultiplier(damageMultiplier);
+  }
+
+  private void FixedUpdate()
+  {
+    rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    upgradeProducts.ProductUpgrade();
+  }
+}
